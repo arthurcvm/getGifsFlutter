09d799ff5142860c8de613bdb63f5aa83a3eb8ec
Arthur Cezar <arthurcvm@hotmail.com>
2021-07-19 08:38:35 -0300

    feat: create interface search field

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,16 @@ import 'package:buscador_gifs/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
   ));
 }
